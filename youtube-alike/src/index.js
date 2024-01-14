a0c46ba0267826aa6693a150395cd7d56f2f7d7f
syedshoaib02 <syeds7933.ss@gmail.com>
2024-01-14 05:40:02 +0530

    db connection establisted

---
+++ youtube-alike/src/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express();
+// (async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//        app.on("error",()=>{
+//         console.log("Err::",error)
+//         throw error
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`App is listenting at ${process.env.PORT}`)
+//        })
+        
+//     } catch (error) {
+//         console.log("Error:",error)
+        
+//     }})()
