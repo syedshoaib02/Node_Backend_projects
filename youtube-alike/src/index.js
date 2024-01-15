848a3447dbacdef5ed696c0ee91d964489a48f81
syedshoaib02 <syeds7933.ss@gmail.com>
2024-01-15 19:24:18 +0530

    utils update

--- youtube-alike/src/index.js
+++ youtube-alike/src/index.js
@@ -4,9 +4,14 @@ import connectDB from './db/index.js';
 dotenv.config({
     path:'./env'
 })
-
-
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT ||8000)
+    console.log(`Server is running at Port:${process.env.PORT}`)
+})
+.catch((err)=>{
+    console.log("err",err)
+})
 
 
 
