1dd32727f645bcb6fc5f3dfed0f1d11df55a4d30
syedshoaib02 <syeds7933.ss@gmail.com>
2024-01-23 17:50:47 +0530

    user logged in code

--- youtube-alike/src/routes/user.routes.js
+++ youtube-alike/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
@@ -19,6 +20,8 @@ router.route("/register").post(
     registerUser
     )
 
+ router.route("/login").post(loginUser)   
 
+router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router;
